18459e20b6dea901e574df947df60d2623db0e67
isurusajith68 <isurusajith68@gmail.com>
2023-11-29 14:54:47 +0530

    google-site-verification

--- src/components/GoogleAnalyticsTracking .js
+++ src/components/GoogleAnalyticsTracking .js
@@ -4,6 +4,7 @@ export const GoogleAnalyticsTracking = () => {
     return (
         <>
             {/* Global site tag (gtag.js) - Google Analytics */}
+            <meta name="google-site-verification" content="UPy462P0DpuVDRDP4ssOZrxL4V6swsQ63HLf-xKXnyM" />
             <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZW3SBNYYSF" />
             <Script id="google-analytics" strategy="afterInteractive">
                 {`
