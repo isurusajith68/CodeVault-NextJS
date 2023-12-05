727a6ed5f2fbb06b2a43fe65317dbd5af8625024
isurusajith68 <isurusajith68@gmail.com>
2023-12-06 00:31:43 +0530

    try to deploy

--- src/app/page.js
+++ src/app/page.js
@@ -176,8 +176,8 @@ const Home = () => {
                       {item.likedBy.length >= 0 && (
                         <div className="cursor-pointer">
                           {item.likedBy.includes(session?.user?.id) ? (
-                            <BiLike
-                              color="blue"
+                            <BiSolidLike
+                              color="red"
                               className=""
                               onClick={() => handleLikeClick(item._id, session?.user?.id)}
                             />
