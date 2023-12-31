92d367896a210d1d80e6cb6905c489208e0a4e03
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2023-12-31 14:55:59 +0530

    d

--- src/app/api/post/[id]/comments/route.js
+++ src/app/api/post/[id]/comments/route.js
@@ -1,22 +1,21 @@
 import Post from "@/model/postModel";
 
 export async function GET(request, { params }) {
-    
-    const id = await params.id
+    const id = await params.id;
+
     try {
-       
         const post = await Post.findById(id).populate('comments');
 
         if (!post) {
+            console.log('Post not found:', id);
             return Response.json({ message: 'Post not found' }, { status: 404 });
         }
-     
+
+        console.log('Comments fetched successfully:', post.comments);
         return Response.json(post.comments);
 
     } catch (error) {
-        console.error(error);
-        return Response.json({ message: 'Internal Server  comment ' }, { status: 500 });
+        console.error('Error fetching comments:', error);
+        return Response.json({ message: 'Internal Server Error' }, { status: 500 });
     }
-
-
 }
