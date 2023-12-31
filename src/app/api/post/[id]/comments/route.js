1f1369d3399754bd75e1456f922b978f93b6d99e
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2023-12-31 14:37:37 +0530

    server error comment section

--- src/app/api/post/[id]/comments/route.js
+++ src/app/api/post/[id]/comments/route.js
@@ -1,21 +1,21 @@
 import Post from "@/model/postModel";
 
 export async function GET(request, { params }) {
-
+    
+    const id = await params.id
     try {
-        const id = await params.id
-        console.log(id)
-
+       
         const post = await Post.findById(id).populate('comments');
+
         if (!post) {
-            return Response.json({ message: 'Post not found' });
+            return Response.json({ message: 'Post not found' }, { status: 404 });
         }
      
         return Response.json(post.comments);
 
     } catch (error) {
         console.error(error);
-        return Response.json({ message: 'Internal Server  comment ' });
+        return Response.json({ message: 'Internal Server  comment ' }, { status: 500 });
     }
 
 
