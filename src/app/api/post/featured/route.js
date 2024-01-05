049583f24701826ee60409c1b694a49789353958
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-01-06 00:46:22 +0530

    x

--- src/app/api/post/featured/route.js
+++ src/app/api/post/featured/route.js
@@ -1,7 +1,7 @@
-import Post from "@/model/postModel"
+// import Post from "@/model/postModel"
 
-export async function GET(request) {
-    const posts = await Post.find().find({ isFeatured: true })
+// export async function GET(request) {
+//     const posts = await Post.find().find({ isFeatured: true })
 
-    return Response.json({ data: posts })
-}
+//     return Response.json({ data: posts })
+// }
